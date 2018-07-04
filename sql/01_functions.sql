82a1b50dba872ca834ca57674c5560fdd3c87e57
Jerome Leclanche <jerome@leclan.ch>
2018-07-04 22:35:00 +0300

    Remove if-exists/or-replace conditionals from function definitions

--- sql/01_functions.sql
+++ sql/01_functions.sql
@@ -1,12 +1,12 @@
 -- json_extract_array_element_text()
-CREATE OR REPLACE FUNCTION json_extract_array_element_text(json_array text, array_index int) RETURNS text immutable as $$
+CREATE FUNCTION json_extract_array_element_text(json_array text, array_index int) RETURNS text immutable as $$
 import json
 result = json.loads(json_array)[array_index]
 return json.dumps(result)
 $$ LANGUAGE plpythonu;
 
 -- json_extract_path_text()
-CREATE OR REPLACE FUNCTION json_extract_path_text(json_string text, VARIADIC path_elems character[]) RETURNS text immutable as $$
+CREATE FUNCTION json_extract_path_text(json_string text, VARIADIC path_elems character[]) RETURNS text immutable as $$
 import json
 result = json.loads(json_string)
 if path_elem not in result: return ""
@@ -15,18 +15,18 @@ return json.dumps(result)
 $$ LANGUAGE plpythonu;
 
 -- json_array_length()
-CREATE OR REPLACE FUNCTION json_array_length(json_array text) RETURNS int immutable as $$
+CREATE FUNCTION json_array_length(json_array text) RETURNS int immutable as $$
 import json
 return len(json.loads(json_array))
 $$ LANGUAGE plpythonu;
 
 -- decode()
-CREATE OR REPLACE FUNCTION decode(expression int, search int, result int, "default" int) RETURNS int immutable as $$
+CREATE FUNCTION decode(expression int, search int, result int, "default" int) RETURNS int immutable as $$
 return result if expression == search else default
 $$ LANGUAGE plpythonu;
 
 -- median()
-CREATE OR REPLACE FUNCTION _final_median(numeric[]) RETURNS numeric immutable as $$
+CREATE FUNCTION _final_median(numeric[]) RETURNS numeric immutable as $$
 	SELECT AVG(val) FROM (
 		SELECT val FROM unnest($1) val
 		ORDER BY 1
@@ -35,7 +35,6 @@ CREATE OR REPLACE FUNCTION _final_median(numeric[]) RETURNS numeric immutable as
 	) sub;
 $$ LANGUAGE sql;
 
-DROP AGGREGATE IF EXISTS median(numeric);
 CREATE AGGREGATE median(numeric) (
 	SFUNC=array_append,
 	STYPE=numeric[],
